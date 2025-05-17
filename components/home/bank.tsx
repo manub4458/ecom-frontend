4847cebf5c0b59b97c29377c69f38af5fa6cd321
Khushi Nagar <nagarkhushi18@gmail.com>
2025-05-17 14:48:11 +0530

    changes

--- components/home/bank.tsx
+++ components/home/bank.tsx
@@ -6,17 +6,17 @@ export function Bank() {
     <Container>
       <div className="space-y-10 pb-20 mt-20">
         <div className="flex flex-col gap-y-4 md:gap-y-12 px-4 sm:px-6 lg:px-8">
-          <h3 className="text-3xl font-bold">Exciting Bank Offers</h3>
-          <div className="flex gap-6 justify-center items-center">
+          <h3 className="text-3xl font-bold ">Exciting Bank Offers</h3>
+          <div className="flex flex-wrap justify-center gap-6">
             <img
               src="/assets/offers/offer1.webp"
-              width={`500px`}
-              className="rounded-lg shadow-xl"
+              className="rounded-lg shadow-xl w-full sm:w-[300px] md:w-[400px] lg:w-[500px]"
+              alt="Bank Offer 1"
             />
             <img
               src="/assets/offers/offer1.webp"
-              width={`500px`}
-              className="rounded-lg shadow-xl"
+              className="rounded-lg shadow-xl w-full sm:w-[300px] md:w-[400px] lg:w-[500px]"
+              alt="Bank Offer 2"
             />
           </div>
         </div>
--- components/store/latestLaunches.tsx
+++ components/store/latestLaunches.tsx
@@ -9,36 +9,37 @@ export const LatestLaunches = () => {
       <div className="py-8">
         <h3 className="text-3xl font-bold">Latest Launches</h3>
       </div>
-      <div className="w-full grid lg:grid-cols-2 my-4 gap-6 md:gap-8 lg:gap-10">
+
+      <div className="w-full grid grid-cols-1 lg:grid-cols-2 my-4 gap-6 md:gap-8 lg:gap-10">
         <div className="w-full aspect-[2/.9] bg-gradient-to-r from-[#C5A090] to-85% to-[#EEE3DF] rounded-lg md:rounded-xl ">
           <img
             src="/assets/latestLaunch.webp"
             alt="Image"
-            className="object-contain bg-blend-color-burn rounded-xl shadow-xl"
+            className="w-full h-full bg-blend-color-burn rounded-xl shadow-xl"
           />
         </div>
         <div className="w-full aspect-[2/.9] bg-gradient-to-r from-[#C5A090] to-85% to-[#EEE3DF] rounded-lg md:rounded-xl ">
           <img
             src="/assets/macbookBanner.webp"
             alt="Image"
-            className="object-contain bg-blend-color-burn rounded-xl shadow-xl"
+            className="w-full h-full  bg-blend-color-burn rounded-xl shadow-xl"
           />
         </div>
       </div>
-      <div className="w-full grid lg:grid-cols-2 my-4 gap-6 md:gap-8 lg:gap-10">
-      
+
+      <div className="w-full grid grid-cols-1 lg:grid-cols-2 my-4 gap-6 md:gap-8 lg:gap-10">
         <div className="w-full aspect-[2/.9] bg-gradient-to-r from-[#C5A090] to-85% to-[#EEE3DF] rounded-lg md:rounded-xl ">
           <img
             src="/assets/macbookBanner.webp"
             alt="Image"
-            className="object-contain bg-blend-color-burn rounded-xl shadow-xl"
+            className="w-full h-full bg-blend-color-burn rounded-xl shadow-xl"
           />
         </div>
         <div className="w-full aspect-[2/.9] bg-gradient-to-r from-[#C5A090] to-85% to-[#EEE3DF] rounded-lg md:rounded-xl ">
           <img
             src="/assets/latestLaunch.webp"
             alt="Image"
-            className="object-contain bg-blend-color-burn rounded-xl shadow-xl"
+            className="w-full h-full  bg-blend-color-burn rounded-xl shadow-xl"
           />
         </div>
       </div>
